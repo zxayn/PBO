2d28315e0975dc9eb9674ba26119b2c8baff105e
redemtusjejeg@gmail.com <zidanxjr8@gmail.com>
2023-04-10 20:41:23 +0700

    asdasd

--- database/desa.sql
+++ database/desa.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.0.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 28 Jan 2023 pada 09.36
--- Versi server: 10.1.32-MariaDB
--- Versi PHP: 5.6.36
+-- Waktu pembuatan: 10 Apr 2023 pada 15.40
+-- Versi server: 10.4.27-MariaDB
+-- Versi PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -34,17 +33,14 @@ CREATE TABLE `data_masyarakat` (
   `nik` char(16) NOT NULL,
   `alamat` text NOT NULL,
   `rt_rw` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `data_masyarakat`
 --
 
 INSERT INTO `data_masyarakat` (`id_masarakat`, `nama`, `nik`, `alamat`, `rt_rw`) VALUES
-(30, 'IRAWAN DIANA', '3601081111990002', 'KP. UMBULAN', '01/02'),
-(31, 'IRAWAN', '3601080202990002', 'KP. UMBULAN', '01/02'),
-(32, 'HAZWAN', '3601085555555555', 'KP. UMBULAN', '01/02'),
-(33, 'satiah', '3601082222222222', 'KP. UMBULAN', '01/02');
+(34, 'Aqilla Ziidane Akbar', '3248886800244346', 'Perum Griyan Intan Permai', 'RT 4/RW 5');
 
 -- --------------------------------------------------------
 
@@ -60,15 +56,15 @@ CREATE TABLE `masyarakat` (
   `username` varchar(25) NOT NULL,
   `password` varchar(32) NOT NULL,
   `tlp` varchar(13) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `masyarakat`
 --
 
 INSERT INTO `masyarakat` (`nik`, `nama`, `alamat`, `rt_rw`, `username`, `password`, `tlp`) VALUES
+('3248886800244346', 'Aqilla Ziidane Akbar', 'Perum Griyan Intan Permai Blok I No.15', 'RT 4/RW 5', 'zidan', 'zidan123', '085608064055'),
 ('3601081111111111', 'IRAWAN', 'KP. KOLECER', '01/02', 'ira', '123', '085780353253'),
-('3601081111990002', 'IRAWAN DIANA', 'KP. UMBULAN', '01/02', 'irawan', '1234', '0857532532632'),
 ('3601082222222222', 'satiah', 'KP. UMBULAN', '01/02', 'sati', '1234', '0857532532646'),
 ('3601085555555555', 'HAZWAN', 'KP. UMBULAN', '01/02', 'hazwan', '123', '0857242552255');
 
@@ -87,18 +83,19 @@ CREATE TABLE `pengaduan` (
   `tlp` varchar(13) NOT NULL,
   `foto` varchar(255) NOT NULL,
   `status` enum('Proses','Selesai') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `pengaduan`
 --
 
 INSERT INTO `pengaduan` (`id_pengaduan`, `tgl_pengaduan`, `nama_pengadu`, `nik`, `isi_laporan`, `tlp`, `foto`, `status`) VALUES
-(36, '2023-01-26 (10:43:52)', 'IRAWAN DIANA', '3601081111990002', 'Terjadi Longgsor di kp. umbulan rt 01/01 dekat POM bensi segera di tindak lanjuti', '0857532532632', 'images (8).jpeg', 'Selesai'),
 (38, '2023-01-27 (09:28:33)', 'IRAWAN', '3601081111111111', 'TELAH TERJADI KEBAKARAN DI RUMAH UDIN RT 01/02 KP. KOLECER MOHON SEGERA DITINDAK LANJUTI', '085780353253', 'images (18).jpeg', 'Selesai'),
 (39, '2023-01-27 (14:23:52)', 'satiah', '3601082222222222', 'TELAH TERJADI BANJIR DI KP. KOLECER 1 ORANG HANYUT BELUM DITEMUKAN MOHON SEGERA DITINDAK LANJUTI...', '0857532532646', 'images (11).jpeg', 'Selesai'),
 (41, '2023-01-28 (08:33:19)', 'HAZWAN', '3601085555555555', 'TELAH TERJADI KEBAKARAN DI DESA UMBULAN RT 01/02 RUMAH PAK RT MOHON SEGERA DI TINDAK LANJUTI.', '0857242552255', 'images (16).jpeg', 'Selesai'),
-(42, '2023-01-28 (08:42:45)', 'HAZWAN', '3601085555555555', 'JALAN DESA KAMPUNG KOLECER RUSAK MOHON SEGERA DIPERBAIKI', '0857242552255', 'images (35).jpeg', 'Proses');
+(70, '2023-04-01 (12:15:44)', 'Aqilla Ziidane Akbar', '3248886800244346', 'Ingin Melaporkan Terkait Jalanan yang rusak untuk Segera Diperbaiki Yang awalnya kecil Jadi Besar kemungkinan Kecelakaan, mohon untuk pihak Dusun Dermo Segera Ditindak lanjuti, Sekian Terima Kasih.', '085608064055', 'images (4).jpeg', 'Selesai'),
+(115, '2023-04-10 (12:51:53)', 'Aqilla Ziidane Akbar', '3248886800244346', 'p', '085608064055', '', 'Proses'),
+(116, '2023-04-10 (20:20:34)', 'Aqilla Ziidane Akbar', '3248886800244346', 'asdsdasd', '085608064055', '', 'Proses');
 
 -- --------------------------------------------------------
 
@@ -113,15 +110,14 @@ CREATE TABLE `petugas` (
   `password` varchar(32) NOT NULL,
   `tlp` varchar(13) NOT NULL,
   `level` enum('admin','petugas') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `petugas`
 --
 
 INSERT INTO `petugas` (`id_petugas`, `nama_petugas`, `username`, `password`, `tlp`, `level`) VALUES
-(1, 'Irawan', 'admin', 'admin', '081617898716', 'admin'),
-(2, 'Diana', 'petugas', 'petugas', '081617898717', 'petugas');
+(1, 'zxayn', 'zxayn', '123', '0856008064055', 'admin');
 
 -- --------------------------------------------------------
 
@@ -136,7 +132,7 @@ CREATE TABLE `tanggapan` (
   `isi_laporan` text NOT NULL,
   `tanggapan` text NOT NULL,
   `id_petugas` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
 --
 -- Dumping data untuk tabel `tanggapan`
@@ -164,7 +160,22 @@ INSERT INTO `tanggapan` (`id_tanggapan`, `id_pengaduan`, `tgl_tanggapan`, `isi_l
 (27, 36, '2023-01-26 (16:10:19)', 'Terjadi Longgsor di kp. umbulan rt 01/01 dekat POM bensi segera di tindak lanjuti', 'vdsbdbdsb', 1),
 (28, 37, '2023-01-27 (09:39:38)', 'TERJADI LONGSOR DI DEKAT RUMAH PAK RT KP KOLECER 1 LUKA BERAT 1 LUKA RINGAN 1 MENINGGAL', 'SIAP SEGERA KAMI AKAN TANGANI KE LOKASI KEJADIAN ', 1),
 (29, 39, '2023-01-27 (14:40:23)', 'TELAH TERJADI BANJIR DI KP. KOLECER 1 ORANG HANYUT BELUM DITEMUKAN MOHON SEGERA DITINDAK LANJUTI...', 'TERIMAKASIH ATAS INFORMASINYA AKAN SEGERA KAMI TINDAK LANJUTI', 1),
-(30, 41, '2023-01-28 (08:40:16)', 'TELAH TERJADI KEBAKARAN DI DESA UMBULAN RT 01/02 RUMAH PAK RT MOHON SEGERA DI TINDAK LANJUTI.', 'TERIMAKASIH ATAS INFORMASINYA AKAN SEGERA KAMAI TINDAK LANJUTI', 1);
+(30, 41, '2023-01-28 (08:40:16)', 'TELAH TERJADI KEBAKARAN DI DESA UMBULAN RT 01/02 RUMAH PAK RT MOHON SEGERA DI TINDAK LANJUTI.', 'TERIMAKASIH ATAS INFORMASINYA AKAN SEGERA KAMAI TINDAK LANJUTI', 1),
+(31, 36, '2023-03-15 (14:04:10)', 'Terjadi Longgsor di kp. umbulan rt 01/01 dekat POM bensi segera di tindak lanjuti', 'asdad', 1),
+(32, 38, '2023-03-15 (14:06:46)', 'TELAH TERJADI KEBAKARAN DI RUMAH UDIN RT 01/02 KP. KOLECER MOHON SEGERA DITINDAK LANJUTI', 'asd', 1),
+(33, 36, '2023-03-15 (14:06:59)', 'Terjadi Longgsor di kp. umbulan rt 01/01 dekat POM bensi segera di tindak lanjuti', 'awokwoakowa', 1),
+(34, 38, '2023-03-15 (14:07:19)', 'TELAH TERJADI KEBAKARAN DI RUMAH UDIN RT 01/02 KP. KOLECER MOHON SEGERA DITINDAK LANJUTI', 'Segera dilakukan', 1),
+(35, 38, '2023-03-15 (14:07:56)', 'TELAH TERJADI KEBAKARAN DI RUMAH UDIN RT 01/02 KP. KOLECER MOHON SEGERA DITINDAK LANJUTI', 'fffffffffffffffffffff', 1),
+(36, 39, '2023-03-15 (14:08:12)', 'TELAH TERJADI BANJIR DI KP. KOLECER 1 ORANG HANYUT BELUM DITEMUKAN MOHON SEGERA DITINDAK LANJUTI...', 'asdasdadasdasdasdd', 1),
+(37, 46, '2023-03-28 (18:08:00)', 'BOYOKKU LORO', 'tolol', 1),
+(38, 46, '2023-03-28 (18:08:13)', 'BOYOKKU LORO', 'tapi boong', 1),
+(39, 57, '2023-03-31 (09:41:56)', 'kurang', 'wwww', 1),
+(40, 59, '2023-03-31 (10:37:03)', 'sksksksk', 'siap', 1),
+(41, 65, '2023-04-01 (11:43:49)', 'zz', 'awkokawokawoaw', 1),
+(42, 69, '2023-04-01 (11:58:01)', 'Kejadian Tawuran di jl. agung', 'Segera Ditangani secepatnya', 1),
+(43, 70, '2023-04-05 (09:12:50)', 'Ingin Melaporkan Terkait Jalanan yang rusak untuk Segera Diperbaiki Yang awalnya kecil Jadi Besar kemungkinan Kecelakaan, mohon untuk pihak Dusun Dermo Segera Ditindak lanjuti, Sekian Terima Kasih.', 'Segera Ditidindak lanjuti Mohon Untuk tunggu kabar Selanjutnya dari pihak Kami', 1),
+(44, 38, '2023-04-10 (01:53:40)', 'TELAH TERJADI KEBAKARAN DI RUMAH UDIN RT 01/02 KP. KOLECER MOHON SEGERA DITINDAK LANJUTI', 'awokwaoawowa', 1),
+(45, 111, '2023-04-10 (12:33:08)', 'asdasd', 'KONTOLLLLLLLLLLLLLLLLLLLLLLLLLLLLL', 1);
 
 --
 -- Indexes for dumped tables
@@ -211,13 +222,13 @@ ALTER TABLE `tanggapan`
 -- AUTO_INCREMENT untuk tabel `data_masyarakat`
 --
 ALTER TABLE `data_masyarakat`
-  MODIFY `id_masarakat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
+  MODIFY `id_masarakat` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
 
 --
 -- AUTO_INCREMENT untuk tabel `pengaduan`
 --
 ALTER TABLE `pengaduan`
-  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `id_pengaduan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=117;
 
 --
 -- AUTO_INCREMENT untuk tabel `petugas`
@@ -229,7 +240,7 @@ ALTER TABLE `petugas`
 -- AUTO_INCREMENT untuk tabel `tanggapan`
 --
 ALTER TABLE `tanggapan`
-  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id_tanggapan` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
